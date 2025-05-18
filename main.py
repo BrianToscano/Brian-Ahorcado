0f46bc117e4bcf793506aa1c6bf7c180fbca007d
Brian Toscano <160367122+BrianToscano@users.noreply.github.com>
2025-05-17 23:25:47 -0700

    Juego Ahorcado
    Ejecutar el main.py para comenzar a jugar a el ahorcado, en el que tendrás que atinar la letra  teniendo 6 oportunidades antes de que el muñeco te aparezca ahorcado

---
+++ main.py
@@ -0,0 +1,11 @@
+from BrianAhorcado import Ahorcado
+from jugador import Jugador
+from tablero import Tablero
+
+
+# Importamos la clase Ahorcado desde el archivo ahorcado.py
+from BrianAhorcado import Ahorcado
+
+if __name__ == "__main__":
+# Iniciamos el juego utilizando la función jugar() de la clase Ahorcado
+    Ahorcado.jugar()
